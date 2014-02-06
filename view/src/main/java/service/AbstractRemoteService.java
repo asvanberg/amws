37b6b32c85c02d53115c4987e41f8d89476bd2f6
Andreas Svanberg <andreass@dsv.su.se>
2014-02-06 16:29:25 +0100

    Caching the JAX-RS client gives huge performance boost.

--- view/src/main/java/service/AbstractRemoteService.java
+++ view/src/main/java/service/AbstractRemoteService.java
@@ -6,9 +6,12 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
 public abstract class AbstractRemoteService {
+
+    private static final WebTarget TARGET = ClientBuilder.newClient()
+            .register(JacksonFeature.class)
+            .target("http://localhost:8081");
+
     protected WebTarget api() {
-        return ClientBuilder.newClient()
-                .register(JacksonFeature.class)
-                .target("http://localhost:8081");
+        return TARGET;
     }
 }
