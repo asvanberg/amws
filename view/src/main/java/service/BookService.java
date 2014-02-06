efd142e53436ac57fcbc7ab0597e0490023591fb
Andreas Svanberg <andreass@dsv.su.se>
2014-02-06 15:21:08 +0100

    Added some simple println() to see where the time is spent.

--- view/src/main/java/service/BookService.java
+++ view/src/main/java/service/BookService.java
@@ -6,10 +6,13 @@ import javax.ws.rs.core.MediaType;
 
 public class BookService extends AbstractRemoteService {
     public Book get(long id) {
-        return api()
+        System.out.println("Before book fetch/parse: " + System.currentTimeMillis() % 10000);
+        Book book = api()
                 .path("book")
                 .path(Long.toString(id))
                 .request(MediaType.APPLICATION_JSON_TYPE)
                 .get(Book.class);
+        System.out.println("After book fetch/parse: " + System.currentTimeMillis() % 10000);
+        return book;
     }
 }
--- view/src/main/java/service/PersonService.java
+++ view/src/main/java/service/PersonService.java
@@ -6,10 +6,13 @@ import javax.ws.rs.core.MediaType;
 
 public class PersonService extends AbstractRemoteService {
     public Person get(long id) {
-        return api()
+        System.out.println("Before person fetch/parse: " + System.currentTimeMillis() % 10000);
+        Person person = api()
                 .path("person")
                 .path(Long.toString(id))
                 .request(MediaType.APPLICATION_JSON_TYPE)
                 .get(Person.class);
+        System.out.println("After person fetch/parse: " + System.currentTimeMillis() % 10000);
+        return person;
     }
 }
